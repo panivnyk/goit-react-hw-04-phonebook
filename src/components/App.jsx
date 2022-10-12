56be20e8c18f1028ebb98a71b25269ff973ad16d
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-10-12 21:37:38 +0300

    copy from previous HW

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,86 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+
+import { Div, Header, PContact, Section, Message } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const saveContacts = localStorage.getItem('contacts');
+    if (JSON.parse(saveContacts)) {
+      this.setState({ contacts: JSON.parse(saveContacts) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleInput = (values, { resetForm }) => {
+    const { name, number } = values;
+    const contact = {
+      name,
+      number,
+    };
+    const dublicateContact = this.findDublicate(contact, this.state.contacts);
+    dublicateContact
+      ? alert(`${contact.name} or ${contact.number} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, { ...values, id: nanoid() }],
+        }));
+    resetForm();
+  };
+
+  findDublicate = (contact, contactsList) => {
+    return contactsList.find(
+      item =>
+        item.name.toLowerCase() === contact.name.toLowerCase() ||
+        item.number === contact.number
+    );
+  };
+
+  getFilterContacts = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  changeFilter = event => {
+    this.setState({
+      filter: event.currentTarget.value,
+    });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const contacts = this.getFilterContacts();
+    return (
+      <Div>
+        <Header>Phonebook</Header>
+        <Section>
+          <ContactForm onSubmit={this.handleInput} />
+        </Section>
+        <PContact>Contacts</PContact>
+        <Filter value={this.state.filter} onValueChange={this.changeFilter} />
+        {contacts.length === 0 && <Message>There is not any contacts</Message>}
+        <ContactList contacts={contacts} deleteContact={this.deleteContact} />
+      </Div>
+    );
+  }
+}
